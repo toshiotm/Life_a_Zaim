92daa2a9acdb6e81ef45c273f36966a744a50528
toshiotm <toshi.otm@gmail.com>
2012-09-29 13:05:58 +0900

    first commit

---
+++ Life_a_Zaim/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeAZaim::Application.config.secret_token = '527531812a1e96040fcdf198e25d49b663de935ebd4fde8b6d788f904a5ed381d4798ff1611d6ffc45bd761c665c06d655ea008a073df1ef0a9509e61223f0ac'
---
+++ Life_a_Zaim/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LifeAZaim::Application.config.session_store :cookie_store, key: '_Life_a_Zaim_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LifeAZaim::Application.config.session_store :active_record_store
