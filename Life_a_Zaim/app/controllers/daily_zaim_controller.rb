3a0ca5b95e0d9b9633807b8ec1668baca0c38eeb
Hiroshi Maekawa <posaunehm@gmail.com>
2012-09-29 15:33:12 +0900

    unit毎の計算を追加

--- Life_a_Zaim/app/controllers/daily_zaim_controller.rb
+++ Life_a_Zaim/app/controllers/daily_zaim_controller.rb
@@ -5,11 +5,20 @@ class DailyZaimController < ApplicationController
   	
   	cashsum = 0
 
-  	@cashflows.each do |cash| 
+  	@cashflows.each do |cash|
+
+  		addCash = cash.amount;
+  		case cash.unit
+			when 3
+				addCash /= 30    
+			when 4
+				addCash /= 365
+		end
+
   		if cash.isIncome 
-  			cashsum += cash.amount
+  			cashsum += addCash
   		else
-  			cashsum -= cash.amount
+  			cashsum -= addCash
   		end
   	end
   	render :text =>  cashsum
