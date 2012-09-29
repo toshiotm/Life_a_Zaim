9dc245389b0fa83c2c4d12b979f51c84d21ab7dc
icchw <kh.wada@gmail.com>
2012-09-29 14:44:42 +0900

    change type of date from datetime to date

--- Life_a_Zaim/db/schema.rb
+++ Life_a_Zaim/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120929050907) do
+ActiveRecord::Schema.define(:version => 20120929053522) do
 
   create_table "cashflows", :force => true do |t|
     t.integer  "amount"
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(:version => 20120929050907) do
     t.integer  "unit"
     t.string   "description"
     t.boolean  "isIncome"
-    t.datetime "date"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.date     "date"
   end
 
 end
