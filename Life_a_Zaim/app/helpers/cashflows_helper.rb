ad0f1594958416c82a0bd67924ee5811290cff41
icchw <kh.wada@gmail.com>
2012-09-29 16:10:21 +0900

    convert unit

--- Life_a_Zaim/app/helpers/cashflows_helper.rb
+++ Life_a_Zaim/app/helpers/cashflows_helper.rb
@@ -2,11 +2,18 @@
 
 module CashflowsHelper
 
+
 	def unit_choices
 		[["１回", 1],
 		["毎日", 2],
 		["毎月", 3],
 		["毎年", 4]
 		]
+	end
+
+	def unit_convert(val)
+          units = { "1"=>"1回", "2"=>"毎日", "3"=>"毎月", "4"=>"毎年"}
+
+          units[val.to_s]
 	end
 end
